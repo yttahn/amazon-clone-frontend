33e4f017f5e4dc7b3c3ebc936eb459fc83d5fce3
natty <nathnaelyirga@gmail.com>
2024-09-28 01:58:44 +0300

    carousel 1st slide works

--- src/Components/Carousel/Carousel.jsx
+++ src/Components/Carousel/Carousel.jsx
@@ -1,27 +1,18 @@
 import React from "react";
-import { Carousel } from "react-responsive-carousel";
 import { imgs } from "../../Utility/data";
-import "react-responsive-carousel/lib/styles/carousel.min.css";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from "react-responsive-carousel";
 import classes from "./Carousel.module.css";
 
 function CarouselEffect() {
   return (
-    <div>
-      <Carousel
-        autoPlay={true}
-        infiniteLoop={true}
-        showIndicators={false}
-        showThumbs={true}
-        interval={3000}
-        stopOnHover={false}
-        dynamicHeight={true}
-      >
-        {imgs.map((imageItemLink, index) => (
-          <img src={imageItemLink} alt={`carousel-item-${index}`} key={index} />
-        ))}
-      </Carousel>
-      <div className={classes.hero__image}></div> {/* Fixed class name */}
-    </div>
+    <Carousel>
+      {imgs.map((imageItem, index) => (
+        <div className={classes.hero__image} key={index}>
+          <img src={imageItem} alt={`carousel-item-${index}`} />
+        </div>
+      ))}
+    </Carousel>
   );
 }
 
